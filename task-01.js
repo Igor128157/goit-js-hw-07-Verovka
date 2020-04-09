d463fbde920bc6c4fa9f9ba98860895be5710f23
Igor Verovka <igor0063@ukr.net>
2020-04-09 19:10:45 +0300

    add next change

--- task-01.js
+++ task-01.js
@@ -1,7 +1,15 @@
 
-const items = document.querySelectorAll('.item');
+const ulCategories = document.querySelectorAll("ul#categories");
+
+Array.prototype.forEach.call(ulCategories, (element) => {
+  const ulCategoriesLenght = element.querySelectorAll("li.item").length;
+  console.log(`В списке ${ulCategoriesLenght} категории`);
+});
+
+const items = document.querySelectorAll(".item");
+
 Array.prototype.forEach.call(items, (element) => {
-    const title = element.querySelector('h2').innerHTML;
-    const itemsLength = element.querySelectorAll('li').length;
-    console.log(`Категория: ${title} / Количество элементов: ${itemsLength}`);
+  const title = element.querySelector("h2").innerHTML;
+  const itemsLength = element.querySelectorAll("li").length;
+  console.log(`Категория: ${title} / Количество элементов: ${itemsLength}`);
 });
